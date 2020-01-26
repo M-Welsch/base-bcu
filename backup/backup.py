723bdcf45da3e743b1d5ed0fd72864cc99ec827c
Maximilian Welsch <maxiwelsch@aol.com>
2020-01-26 22:00:31 +0000

    fixed output stream issue in 'run_external_command_as_generator' in backup.py

--- backup/backup.py
+++ backup/backup.py
@@ -1,11 +1,11 @@
-from time import sleep
+from time import sleep, time
 from threading import Thread
 
 # from base.common.utils import run_external_command_as_generator
 from subprocess import run, Popen, PIPE, STDOUT
 def run_external_command_as_generator(command):
-	p = run(command, stdout=PIPE, stderr=PIPE)
-	return iter(p.stdout.splitlines, b'')
+	p = Popen(command, stdout=PIPE, stderr=PIPE, bufsize=1, universal_newlines=True)
+	return p.stdout
 
 
 class BackupManager:
@@ -15,20 +15,24 @@ class BackupManager:
 		self._backup_thread = None
 
 	def backup(self):
-		self._backup_thread = BackupThread(self._backup_config["loop_interval"])
+		self._backup_thread = BackupThread(self._backup_config["sample_interval"])
 		self._backup_thread.start()
 
 
 class BackupThread(Thread):
-	def __init__(self, loop_interval):
+	def __init__(self, sample_interval):
 		super(BackupThread, self).__init__()
-		self._loop_interval = loop_interval
+		self._sample_interval = sample_interval
 
 	def run(self):
-		for line in run_external_command_as_generator(["ls", "-a"]):
-			print(line)
-			# show on display
-			sleep(self._loop_interval)
+		start = time()
+		# for line in run_external_command_as_generator(["find", "/"]):
+		for line in run_external_command_as_generator(["grep", "-r", "-i", "e", "/home"]):
+			now = time()
+			if now - start >= self._sample_interval:
+				print(line.strip())
+				# show on display
+				start = now
 
 	def terminate(self):
 		# kill rsync
@@ -36,5 +40,5 @@ class BackupThread(Thread):
 
 
 if __name__ == "__main__":
-	bm = BackupManager({"loop_interval": 1}, None)
+	bm = BackupManager({"sample_interval": 0.2}, None)
 	bm.backup()
