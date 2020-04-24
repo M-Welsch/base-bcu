82a7b2da7ede5824e1d756a94c518b679216db13
Maximilian Welsch <maxiwelsch@posteo.de>
2020-04-24 12:04:05 +0100

    worked on stepper_tester

--- hwctrl/test_programs/test_stepper.py
+++ hwctrl/test_programs/test_stepper.py
@@ -3,18 +3,19 @@ path_to_module = "/home/maxi"
 sys.path.append(path_to_module)
 
 from base.hwctrl.hwctrl import HWCTRL
+from base.common.base_logging import Logger
+from base.common.config import Config
 
 class Stepper_Tester():
-	def test_docking():
-		_hardware_control.get_hw_revision()
-		# _hardware_control.dock()
+	def test_docking(self):
+		_hardware_control.dock()
 
 
 if __name__ == "__main__":
 	path_to_module = "/home/maxi"
 	sys.path.append(path_to_module)
-	_config = Config("base/config.json")
-	_scheduler = BaseScheduler()
-	_logger = Logger(_config.logs_directory)
-	._hardware_control = HWCTRL(_config.hwctrl_config, _logger)
+	_config = Config("/home/maxi/base/config.json")
+	_logger = Logger("/home/maxi/base/log")
+	_hardware_control = HWCTRL(_config.hwctrl_config, _logger)
 	ST = Stepper_Tester()
+	ST.test_docking()
