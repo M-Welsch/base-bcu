b1c30a81349a355e2b841c8060acaa53af4c7575
Maximilian Welsch <maxiwelsch@posteo.de>
2020-10-12 23:06:01 +0200

    extended setup.sh with programming the standby unit

--- setup2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo chmod +x /home/base/base/setup_files/copy_service_and_nginx_files.sh
-sudo chmod +x /home/base/base/setup_files/create_aliases.sh
-/home/base/base/setup_files/copy_service_and_nginx_files.sh
-/home/base/base/setup_files/create_aliases.sh
