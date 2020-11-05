29d436da119ec02fb803897075928ce43ff22e2c
Maximilian Welsch <maxiwelsch@posteo.de>
2020-11-05 11:11:51 +0100

    minor bugfix in scheduler

---
+++ test/common_nas_finder/test_nas_hdd_mounted.py
@@ -0,0 +1,20 @@
+import os, sys
+
+path_to_module = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+print(path_to_module)
+sys.path.append(path_to_module)
+
+from base.common.config import Config
+from base.common.base_logging import Logger
+from base.common.nas_finder import NasFinder
+
+if __name__ == '__main__':
+    config = Config("/home/base/base/config.json")
+    logger = Logger('.')
+    NF = NasFinder(logger, config.config_backup)
+    nas_ip = config.config_backup["ssh_host"]
+    nas_user = config.config_backup["ssh_user"]
+    print(NF._nas_hdd_mounted(nas_ip, nas_user))
+    wrong_ip = '192.168.1.1'
+    print(NF._nas_hdd_mounted(wrong_ip, nas_user))
+    logger.terminate()
