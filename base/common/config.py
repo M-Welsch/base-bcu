887e39f326860e06a2a68f6272ac6659e1f2b903
Maximilian Staab <Specht888@aol.com>
2020-11-22 22:44:23 +0100

    Fix Config

--- base/common/config.py
+++ base/common/config.py
@@ -6,9 +6,8 @@ class Config(dict):
 	def __init__(self, config_path: str, read_only: bool = True, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self._read_only: bool = read_only
-		self._initialized: bool = False
 		self._config_path: str = config_path
-		self._initialized = True
+		self._initialized: bool = True
 		self.reload()
 
 	def reload(self) -> None:
@@ -37,7 +36,7 @@ class Config(dict):
 			raise RuntimeError(f"'{type(self).__name__}' object is read-only")
 		elif name in self.keys() and not self._read_only:
 			self[name] = value
-		elif name not in self.keys() and not self._initialized:
+		elif name not in self.keys() and "_initialized" not in self.__dict__:
 			self.__dict__[name] = value
 		else:
 			raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
