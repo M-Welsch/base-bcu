4e5acb0c5b4f63bcbf4e3dc469bbed698f8744ee
Maximilian Staab <Specht888@aol.com>
2020-12-18 00:16:50 +0100

    Rewrite Schedule: now using sched module instead of schedule module

--- base/logic/schedule.py
+++ base/logic/schedule.py
@@ -1,35 +1,31 @@
-from schedule import Scheduler, Job, CancelJob
+import sched
+from time import time, sleep
+
 from signalslot import Signal
 
-from base.logic.backup import Backup
 from base.common.config import Config
 
 
-class Schedule(Scheduler):
-    valid_backup_frequencies = {"hours", "days", "weeks"}
-    valid_days_of_week = {"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"}
+class Schedule:
+    valid_backup_frequencies = {"hours", "days", "weeks", "months"}
+    # valid_days_of_week = {"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"}
+    valid_days_of_week = {0, 1, 2, 3, 4, 5, 6}
     shutdown_request = Signal()
+    backup_request = Signal()
 
     def __init__(self):
-        super().__init__()
-        self._config = Config("python.base/base/config/schedule_config.json")
+        self._scheduler = sched.scheduler(time, sleep)
+        self._config = Config("schedule_config.json")
         self._config.assert_keys({"shutdown_delay_minutes"})
-        self._schedule = Config("python.base/base/config/schedule_backup.json")
-        self._schedule.assert_keys({"backup_frequency"})
+        self._schedule = Config("schedule_backup.json")
+        self._schedule.assert_keys({"backup_frequency", "day_of_week"})
+        self._backup_job = None
 
-        self._backup_slot = None
+    def run_pending(self):
+        self._scheduler.run(blocking=False)
 
-        self._shutdown_slot = Job(self._config.shutdown_delay_minutes, scheduler=self)
-        self._shutdown_slot.unit = "minutes"
-
-        self._reload_schedule_slot = Job(15, scheduler=self)
-        self._reload_schedule_slot.unit = "minutes"
-
-        self._reconfig_slot = Job(1, scheduler=self)
-        self._reconfig_slot.unit = "seconds"
-
-
-    def load(self):
+    def on_schedule_changed(self, **kwargs):
+        self._schedule.reload()
         backup_frequency = self._schedule.backup_frequency
         day_of_week = self._schedule.day_of_week
         if backup_frequency not in Schedule.valid_backup_frequencies:
@@ -42,34 +38,23 @@ class Schedule(Scheduler):
                 f"{day_of_week} is no valid day!"
                 f"Use one of {Schedule.valid_days_of_week}"
             )
-        self._backup_slot = Job(1, scheduler=self)
-        self._backup_slot.unit = backup_frequency
-        self._backup_slot.start_day = day_of_week
-        self._backup_slot.hour = self._schedule.hour
-        self._backup_slot.minute = self._schedule.minute
+        if self._backup_job is not None:
+            self._scheduler.cancel(self._backup_job)
+        due = None  # TODO: calculate due
+        self._backup_job = self._scheduler.enterabs(due, 2, self._invoke_backup)
 
-    "@Slot()"
+    def _invoke_backup(self):
+        self.backup_request.emit()
+        due = None  # TODO: calculate due
+        self._backup_job = self._scheduler.enterabs(due, 2, lambda: self._invoke_backup())  # TODO: lambda necessary?
+
     def on_reconfig(self, new_config, **kwargs):
-        self._reconfig_slot.do(lambda: self._reconfig_task(new_config))
+        self._scheduler.enter(1, 1, lambda: self._reconfig(new_config))
 
     @staticmethod
-    def _reconfig_task(new_config):
-        # actually do something with new_config
-        return CancelJob
+    def _reconfig(new_config):
+        print("Reconfiguring...")  # TODO: actually do something with new_config
 
-    "@Slot()"
-    def on_shutdown(self, **kwargs):
-        self._shutdown_slot.do(self._shutdown_task)
-
-    def _shutdown_task(self):
-        self.shutdown_request.emit()
-        return CancelJob
-
-
-
-"""
-- backup
-- shutdown
-- reload schedule
-- validate and set configs
-"""
+    def on_shutdown_requested(self, **kwargs):
+        delay = self._config.shutdown_delay_minutes
+        self._scheduler.enter(delay, 1, lambda: self.shutdown_request.emit())  # TODO: lambda necessary?
