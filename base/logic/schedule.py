5c15b0051b9752b70a140b3bce3973a6797513cf
Maximilian Staab <Specht888@aol.com>
2020-12-20 15:13:29 +0100

    Use new TimeCalculator to determine time for next backup, some refactoring

--- base/logic/schedule.py
+++ base/logic/schedule.py
@@ -1,9 +1,11 @@
 import sched
 from time import time, sleep
+from typing import Optional
 
 from signalslot import Signal
 
 from base.common.config import Config
+from base.common.time_calculations import TimeCalculator
 
 
 class Schedule:
@@ -13,18 +15,18 @@ class Schedule:
     shutdown_request = Signal()
     backup_request = Signal()
 
-    def __init__(self):
-        self._scheduler = sched.scheduler(time, sleep)
-        self._config = Config("schedule_config.json")
+    def __init__(self) -> None:
+        self._scheduler: sched.scheduler = sched.scheduler(time, sleep)
+        self._config: Config = Config("schedule_config.json")
         self._config.assert_keys({"shutdown_delay_minutes"})
-        self._schedule = Config("schedule_backup.json")
+        self._schedule: Config = Config("schedule_backup.json")
         self._schedule.assert_keys({"backup_frequency", "day_of_week"})
-        self._backup_job = None
+        self._backup_job: Optional[sched.Event] = None
 
-    def run_pending(self):
+    def run_pending(self) -> None:
         self._scheduler.run(blocking=False)
 
-    def on_schedule_changed(self, **kwargs):
+    def on_schedule_changed(self, **kwargs) -> None:
         self._schedule.reload()
         backup_frequency = self._schedule.backup_frequency
         day_of_week = self._schedule.day_of_week
@@ -40,21 +42,23 @@ class Schedule:
             )
         if self._backup_job is not None:
             self._scheduler.cancel(self._backup_job)
-        due = None  # TODO: calculate due
+        self._reschedule_backup()
+
+    def _invoke_backup(self) -> None:
+        self.backup_request.emit()
+        self._reschedule_backup()
+
+    def _reschedule_backup(self) -> None:
+        due = TimeCalculator().next_backup(self._schedule)
         self._backup_job = self._scheduler.enterabs(due, 2, self._invoke_backup)
 
-    def _invoke_backup(self):
-        self.backup_request.emit()
-        due = None  # TODO: calculate due
-        self._backup_job = self._scheduler.enterabs(due, 2, lambda: self._invoke_backup())  # TODO: lambda necessary?
-
-    def on_reconfig(self, new_config, **kwargs):
+    def on_reconfig(self, new_config, **kwargs) -> None:
         self._scheduler.enter(1, 1, lambda: self._reconfig(new_config))
 
     @staticmethod
-    def _reconfig(new_config):
-        print("Reconfiguring...")  # TODO: actually do something with new_config
+    def _reconfig(new_config) -> None:
+        print(f"Reconfiguring according to {new_config}...")  # TODO: actually do something with new_config
 
-    def on_shutdown_requested(self, **kwargs):
+    def on_shutdown_requested(self, **kwargs) -> None:
         delay = self._config.shutdown_delay_minutes
-        self._scheduler.enter(delay, 1, lambda: self.shutdown_request.emit())  # TODO: lambda necessary?
+        self._scheduler.enter(delay, 1, self.shutdown_request.emit)
