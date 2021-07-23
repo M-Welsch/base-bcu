3ca812ee4ef530f99ee78b7a0defe2c2019b8aa7
M.Staab <M.Staab@intence.de>
2021-07-23 22:33:02 +0200

    Fix logger initialization for tests

--- test/conftest.py
+++ test/conftest.py
@@ -1,22 +1,12 @@
-import logging
-import sys
-from datetime import datetime
 from pathlib import Path
 
-import pytest
 
+def pytest_configure(config):
+    """
+    Allows plugins and conftest files to perform initial configuration.
+    This hook is called for every plugin and initial conftest
+    file after command line options have been parsed.
+    """
+    from base.common.logger import LoggerFactory
 
-@pytest.fixture(autouse=True, scope="session")
-def configure_logger(tmpdir_factory):
-    tmpdir = tmpdir_factory.mktemp("logs")
-    logging.basicConfig(
-        filename=Path(tmpdir) / datetime.now().strftime("%Y-%m-%d_%H-%M-%S.log"),
-        level=logging.DEBUG,
-        format="%(asctime)s %(levelname)s: %(name)s: %(message)s",
-        datefmt="%m.%d.%Y %H:%M:%S",
-    )
-    logger = logging.getLogger()
-    logger.addHandler(logging.StreamHandler(sys.stdout))
-    logger.setLevel(logging.DEBUG)
-    logger.info(f"I'm here! tmpdir = {tmpdir}")
-    yield {"tmpdir": str(tmpdir)}
+    LoggerFactory(Path.cwd() / "python.base/base/config", "BaSe_test", development_mode=True)
