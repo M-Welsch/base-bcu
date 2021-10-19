15bf18292ae70e24ac187b2552fb817f9e2e3b4e
Maximilian <MaximilianStaab@gmx.net>
2021-10-19 21:54:43 +0200

    Split Config into Config base class and BoundConfig subclass - Config is now for creating config objects from dicts (for convenient testing) - BoundConfig is the "new" Config for loading configs from json files

--- base/common/config.py
+++ base/common/config.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import json
 from pathlib import Path
-from typing import Any, Set
+from typing import Any, Dict, Set
 from weakref import WeakValueDictionary
 
 from base.common.logger import LoggerFactory
@@ -15,18 +15,53 @@ class ConfigValidationError(Exception):
 
 
 class Config(dict):
-    base_path = Path("base/config/")
-    __instances: WeakValueDictionary[str, Config] = WeakValueDictionary()
+    def __init__(self, data: Dict[str, Any], read_only: bool = True, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self._read_only: bool = read_only
+        self._initialized: bool = True
+        self.update(data)
 
-    def __new__(cls, config_file_name: str, *args: Any, **kwargs: Any) -> Config:
+    def assert_keys(self, keys: Set[str]) -> None:
+        missing_keys = keys - set(self.keys())
+        if missing_keys:
+            raise ConfigValidationError(f"Keys {missing_keys} are missing.")
+
+    @property
+    def is_read_only(self) -> bool:
+        return self._read_only
+
+    def __getattr__(self, name: str) -> Any:
+        if name in self.keys():
+            return self[name]
+        try:
+            return self.__dict__[name]
+        except KeyError:
+            raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if name in self.keys() and self._read_only:
+            raise RuntimeError(f"'{type(self).__name__}' object is read-only")
+        elif name in self.keys() and not self._read_only:
+            self[name] = value
+        elif name not in self.keys() and "_initialized" not in self.__dict__:
+            self.__dict__[name] = value
+        else:
+            raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
+
+
+class BoundConfig(Config):
+    base_path = Path("base/config/")
+    __instances: WeakValueDictionary[str, BoundConfig] = WeakValueDictionary()
+
+    def __new__(cls, config_file_name: str, *args: Any, **kwargs: Any) -> BoundConfig:
         if config_file_name in cls.__instances:
             return cls.__instances[config_file_name]
-        self: Config = super().__new__(cls)
+        self: BoundConfig = super().__new__(cls)
         cls.__instances[config_file_name] = self
         return self
 
     def __init__(self, config_file_name: str, read_only: bool = True, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
+        super(Config, self).__init__(*args, **kwargs)
         self._read_only: bool = read_only
         self._config_path: Path = self.base_path / config_file_name
         self._initialized: bool = True
@@ -54,26 +89,3 @@ class Config(dict):
         missing_keys = keys - set(self.keys())
         if missing_keys:
             raise ConfigValidationError(f"Keys {missing_keys} are missing in {self._config_path}.")
-
-    @property
-    def is_read_only(self) -> bool:
-        return self._read_only
-
-    def __getattr__(self, name: str) -> Any:
-        if name in self.keys():
-            return self[name]
-        else:
-            try:
-                return self.__dict__[name]
-            except KeyError:
-                raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
-
-    def __setattr__(self, name: str, value: Any) -> None:
-        if name in self.keys() and self._read_only:
-            raise RuntimeError(f"'{type(self).__name__}' object is read-only")
-        elif name in self.keys() and not self._read_only:
-            self[name] = value
-        elif name not in self.keys() and "_initialized" not in self.__dict__:
-            self.__dict__[name] = value
-        else:
-            raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
