a1c4cae87cecf59609d5d506b347f15404e1041b
Maximilian Welsch <maxiwelsch@posteo.de>
2022-04-03 11:29:47 +0200

    fix sbu unit tests

---
+++ base/hardware/platform.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from enum import Enum
+from platform import machine
+
+
+class BaSePlatform(Enum):
+    BANANAPI = "BANANAPI"
+    PC = "PC"
+
+
+def who_am_i() -> BaSePlatform:
+    if machine() == "armv7l":
+        i_am = BaSePlatform("BANANAPI")
+    else:
+        i_am = BaSePlatform("PC")
+    return i_am
