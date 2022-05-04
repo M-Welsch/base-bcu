7e3ee39d125d1566b034ed134f0146583c986b60
Maximilian Welsch <maxiwelsch@posteo.de>
2022-05-04 21:28:18 +0200

    write test for mailer.py

--- base/common/mailer.py
+++ base/common/mailer.py
@@ -6,7 +6,6 @@ from yagmail import SMTP
 from yagmail.error import YagAddressError, YagInvalidEmailAddress
 
 from base.common.config import get_config
-from base.common.exceptions import CriticalException
 from base.common.logger import LoggerFactory, most_recent_logfile
 
 LOG = LoggerFactory.get_logger(__name__)
@@ -15,6 +14,7 @@ LOG = LoggerFactory.get_logger(__name__)
 class Mailer:
     def __init__(self) -> None:
         self._receivers: List[str] = get_config("base.json").email_notification_receivers
+        self._critical_messages = LoggerFactory.get_critical_messages()
 
     def send_summary(self) -> None:
         try:
@@ -25,11 +25,11 @@ class Mailer:
                 contents=self._compose_email_body(),
                 attachments=most_recent_logfile(),
             )
-        except (YagAddressError, YagInvalidEmailAddress) as e:
-            raise CriticalException from e
+        except Exception as e:
+            LOG.critical(f"Error occurent during sending summary: {e}")
 
     def _last_backup_ok(self) -> bool:
-        return True
+        return not bool(self._critical_messages)
 
     def _compose_email_subject(self) -> str:
         if self._last_backup_ok():
@@ -39,4 +39,11 @@ class Mailer:
         return "Backup Server Email Notification: " + report
 
     def _compose_email_body(self) -> str:
-        return "Lorem Ipsum ..."
+        if self._last_backup_ok():
+            body = """Backup conducted successfully!"""
+        else:
+            critical_messages = "\n".join(list(self._critical_messages))
+            body = f"""Backup program produced critical errors:
+{critical_messages}
+"""
+        return body
