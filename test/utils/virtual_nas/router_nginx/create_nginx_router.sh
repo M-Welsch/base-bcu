b59b8fcd6e3c06eb2e111d7ec5722cb1e38f4ede
Maximilian Welsch <maxiwelsch@aol.com>
2022-12-05 22:31:13 +0100

    virtual_nas: redirect traffic to rsynd also works

--- test/utils/virtual_nas/router_nginx/create_nginx_router.sh
+++ test/utils/virtual_nas/router_nginx/create_nginx_router.sh
@@ -6,9 +6,16 @@ stream {
   upstream ssh {
     server $IP_ADDRESS_SSH_SERVER:22;
   }
+  upstream rsyncd {
+    server $IP_ADDRESS_RSYNC_SERVER:$RSYNC_DAEMON_PORT;
+  }
   server {
     listen 22;
     proxy_pass ssh;
+  }
+  server {
+    listen $RSYNC_DAEMON_PORT;
+    proxy_pass rsyncd;
   }
 }
 " >> $NGINX_CONFIG_FILE
